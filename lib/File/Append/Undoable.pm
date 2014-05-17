8031680f97b17da1d0859439c3b7c35e3c53ce03
Steven Haryanto <stevenharyanto@gmail.com>
2014-05-17 05:04:08 +0000

    Import of SHARYANTO/File-Append-Undoable-0.02 from CPAN.
    gitpan-cpan-distribution: File-Append-Undoable
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/File-Append-Undoable-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/File/Append/Undoable.pm
+++ lib/File/Append/Undoable.pm
@@ -1,6 +1,6 @@
 package File::Append::Undoable;
 
-use 5.010;
+use 5.010001;
 use strict;
 use warnings;
 use Log::Any '$log';
@@ -8,7 +8,7 @@ use Log::Any '$log';
 use File::Trash::Undoable;
 use File::Copy;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 our %SPEC;
 
@@ -122,9 +122,11 @@ sub append {
 1;
 # ABSTRACT: Append string to a file, with undo support
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -132,21 +134,10 @@ File::Append::Undoable - Append string to a file, with undo support
 
 =head1 VERSION
 
-version 0.01
-
-=head1 SEE ALSO
-
-L<Rinci::Transaction>
-
-=head1 DESCRIPTION
-
-
-This module has L<Rinci> metadata.
+This document describes version 0.02 of File::Append::Undoable (from Perl distribution File-Append-Undoable), released on 2014-05-17.
 
 =head1 FUNCTIONS
 
-
-None are exported by default, but they are exportable.
 
 =head2 append(%args) -> [status, msg, result, meta]
 
@@ -241,7 +232,34 @@ For more information on transaction, see L<Rinci::Transaction>.
 
 Return value:
 
-Returns an enveloped result (an array). First element (status) is an integer containing HTTP status code (200 means OK, 4xx caller error, 5xx function error). Second element (msg) is a string containing error message, or 'OK' if status is 200. Third element (result) is optional, the actual result. Fourth element (meta) is called result metadata and is optional, a hash that contains extra information.
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+=head1 SEE ALSO
+
+L<Rinci::Transaction>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/File-Append-Undoable>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-File-Append-Undoable>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=File-Append-Undoable>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -249,10 +267,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
